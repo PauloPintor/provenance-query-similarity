2460c049207089fcdbbd3864e3ed96cc0a217006
Paulo Pintor <12814875+PauloPintor@users.noreply.github.com>
2026-03-16 10:45:39 +0000

    Queries versions

---
+++ q82_versions/query82_s665.sql
@@ -0,0 +1,18 @@
+
+select  i_item_id
+       ,i_item_desc
+       ,i_current_price
+       ,array_agg(list_value(item.prov, inventory.prov, date_dim.prov, store_sales.prov)) prov
+ from item, inventory, date_dim, store_sales
+ where i_current_price between 8 and 8+30
+ and inv_item_sk = i_item_sk
+ and d_date_sk=inv_date_sk
+ and d_date between cast('2001-04-08' as date) and cast('2001-04-08' as date) + INTERVAL 60 DAY
+ and i_manufact_id in (267,421,753,305)
+ and inv_quantity_on_hand between 100 and 500
+ and ss_item_sk = i_item_sk
+ group by i_item_id,i_item_desc,i_current_price
+ order by i_item_id
+ LIMIT 100;
+
+
